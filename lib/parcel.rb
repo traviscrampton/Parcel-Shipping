1c5f79f52afae73b8d8033f967046b22f053efa8
Travis Crampton & Jake Glassman <student@epicodus.com>
2015-08-10 14:02:47 -0700

    returns cost of shipping without fast shipping

--- lib/parcel.rb
+++ lib/parcel.rb
@@ -4,8 +4,14 @@ class Parcel
     @weight = weight
   end
 
-  define_method(:shipping) do
-    @volume
-    
+  define_method(:shipping) do |distance|
+      volume_price = @volume*(0.10)
+      weight_price = @weight
+      distance_price = distance * (0.30)
+      speed_price = 3*(volume_price + weight_price + distance_price)
+      price = volume_price + weight_price + distance_price
+      price
+    # @volume
+    # @weight
   end
 end
--- spec/parcel_spec.rb
+++ spec/parcel_spec.rb
@@ -3,9 +3,17 @@ require('parcel')
 
 describe('Parcel') do
   describe('#shipping') do
-    it('returns volume based on height width and length') do
+   it('returns volume based on height width and length') do
       test_parcel=Parcel.new(5,10,3,20)
       expect(test_parcel.shipping()).to(eq(150))
+    end
+    it('returns the weight') do
+      test_parcel=Parcel.new(5, 10, 3, 20)
+      expect(test_parcel.shipping()).to(eq(20))
+    end
+    it('returns the cost of shipping without speed') do
+      test_parcel=Parcel.new(5, 10, 3, 20)
+      expect(test_parcel.shipping(50)).to(eq(50))
     end
   end
 end
